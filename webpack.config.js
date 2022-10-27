3dad2ed242600070e35b5b5e67e7e3f110e32d38
Liana <l.yukhymchak@gmail.com>
2022-10-28 00:35:12 +0200

    ls_7 install webpack, create webpack config file, add plugins for html and css, add loaders for fonts and images

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  context: path.resolve(__dirname, "./html_css"),
+
+  entry: {
+    main: ["./js/index.js", "./css/style.css"],
+  },
+
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "build"),
+    },
+    port: 7777,
+    open: true,
+    compress: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(ttf|woff)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[name][ext]",
+        },
+      },
+      {
+        test: /\.(png|jpg|svg)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name][ext]",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin()],
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].min.css",
+    }),
+
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+
+    new CleanWebpackPlugin(),
+
+    new CopyPlugin({
+      patterns: [{ from: "images", to: "images" }],
+    }),
+  ],
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+};
