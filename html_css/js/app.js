a2eefca36b85cb4b404698bc231dd73205367308
Liana <l.yukhymchak@gmail.com>
2022-10-28 12:58:27 +0200

    ls_9 add class Select, add method to create markup select element

--- html_css/js/app.js
+++ html_css/js/app.js
@@ -2,6 +2,7 @@ import { paginator } from "./paginator.js";
 import { Slider } from "./slider.js";
 import { SlickSlider } from "./slick-slider.js";
 import { Storage } from "./storage.js";
+import { Select } from "./select.js";
 import { formChangeData, formOnLoad } from "./form.js";
 
 export class App {
@@ -12,6 +13,9 @@ export class App {
     formChangeData();
 
     const storage = new Storage();
+
+    const select = new Select("select");
+
     paginator("paginator", storage.getDatafromLocalStorage("paginator"));
 
     const slider = new Slider(
---
+++ html_css/js/select.js
@@ -0,0 +1,26 @@
+export class Select{
+    constructor(id) {
+        this.init(id);
+     }
+
+    init(id) {
+        const select = document.getElementById(id);
+        select.appendChild(this.createSelectItemMarkup());
+    }
+
+    createSelectItemMarkup() { 
+        const select = document.createElement("select");
+        select.classList.add("preference__select");
+
+        for (let i = 0; i < 3; i++) {
+            const option = document.createElement("option");
+            option.setAttribute("value", `item-${i}`)
+            option.innerHTML = `Item ${i + 1}`;
+            
+            select.appendChild(option);
+        }
+
+        return select;
+    }
+}
+
