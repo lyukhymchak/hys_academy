e1f95a7b90782b1b678088d5dc86b37a0e28b1c2
Liana <l.yukhymchak@gmail.com>
2022-11-09 19:22:32 +0100

    ls_7 change Form class, add form validation

--- html_css/js/form.js
+++ html_css/js/form.js
@@ -1,48 +1,95 @@
 export class Form {
   constructor(id) {
     const FORM_DATA_KEY = id + "Data";
+
     this.form = document.getElementById(id);
-    this.name = form.querySelector("input[type=text]");
-    this.tel = form.querySelector("input[type=tel]");
-    this.email = form.querySelector("input[type=email]");
+
+    this.inputs = Array.from(this.form.elements).filter(
+      (el) => el.nodeName === "INPUT"
+    );
 
     this.retrieveRecords(FORM_DATA_KEY);
 
-    form.addEventListener("input", () => this.storeRecords(FORM_DATA_KEY));
-    form.addEventListener("submit", (event, key) =>
-      this.clearStorage(event, FORM_DATA_KEY)
+    this.form.setAttribute("novalidate", true);
+
+    for (const inputElement of this.inputs) {
+      inputElement.addEventListener("input", (e) => {
+        this.markFieldAsError(e.target, !e.target.checkValidity());
+      });
+    }
+
+    this.form.addEventListener("submit", (event) =>
+      this.validateForm(event, FORM_DATA_KEY)
     );
+
+    this.form.addEventListener("input", () => this.storeRecords(FORM_DATA_KEY));
   }
 
   retrieveRecords(key) {
     const data = JSON.parse(localStorage.getItem(key));
 
     if (data !== null) {
-      this.name.value = data.name;
-      this.tel.value = data.tel;
-      this.email.value = data.email;
+      this.inputs.forEach((element, index) => {
+        element.value = data[index][1];
+      });
     }
   }
 
   clearStorage(event, key) {
-    event.preventDefault();
-
     localStorage.removeItem(key);
-
-    this.name.value = "";
-    this.tel.value = "";
-    this.email.value = "";
   }
 
   storeRecords(key) {
-    const data = {
-      name: this.name.value,
-      tel: this.tel.value,
-      email: email.value,
-    };
+    const data = [];
+
+    this.inputs.forEach((element) => {
+      data.push([element.name, element.value]);
+    });
 
     localStorage.setItem(key, JSON.stringify(data));
   }
-}
 
-// export { formOnLoad };
+  validateForm(e, key) {
+    e.preventDefault();
+
+    let formErrors = false;
+    const form = e.target;
+    const field = Array.from(form.elements);
+
+    for (const el of field) {
+      this.markFieldAsError(el, false);
+      this.toggleErrorField(el, false);
+
+      if (!el.checkValidity()) {
+        this.markFieldAsError(el, true);
+        this.toggleErrorField(el, true);
+        formErrors = true;
+      }
+    }
+
+    if (!formErrors) {
+      form.submit();
+      this.clearStorage(e, key);
+    }
+  }
+
+  toggleErrorField(field, show) {
+    const errorText = field.previousElementSibling;
+
+    if (errorText !== null) {
+      if (errorText.classList.contains("form-error-text")) {
+        errorText.style.display = show ? "block" : "none";
+        errorText.setAttribute("aria-hidden", show);
+      }
+    }
+  }
+
+  markFieldAsError(field, show) {
+    if (show) {
+      field.classList.add("field-error");
+    } else {
+      field.classList.remove("field-error");
+      this.toggleErrorField(field, false);
+    }
+  }
+}
